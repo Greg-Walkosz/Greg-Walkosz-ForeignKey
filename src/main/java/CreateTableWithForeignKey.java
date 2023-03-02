6b362075abe8c2c3401789ef239f21e8b50ccb55
Greg Walkosz <walkoszg1@gmail.com>
2023-03-02 20:40:25 +0000

    Completed Foreign Key

--- src/main/java/CreateTableWithForeignKey.java
+++ src/main/java/CreateTableWithForeignKey.java
@@ -90,6 +90,8 @@ public class CreateTableWithForeignKey {
          */
         String sql = FileUtil.parseSQLFile("problem1.sql");
 
+        sql = "CREATE TABLE post (id SERIAL PRIMARY KEY, post varchar(255), user_fk int, FOREIGN KEY (user_fk) REFERENCES site_user(id))";
+
         try {
             Connection connection = ConnectionUtil.getConnection();
             Statement s = connection.createStatement();
